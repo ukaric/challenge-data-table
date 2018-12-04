11798a65f4f17baab04990e623b7afaacf0b766b
Uros Karic <dot.kadza@gmail.com>
2018-12-04 01:53:13 +0100

    :feelsgood:

--- report.jsx
+++ report.jsx
@@ -2,10 +2,58 @@ var React = require('react')
 var ReactPivot = require('react-pivot')
 var createReactClass = require('create-react-class')
 
-var rows = require('./data.json')
+const rows = require('./data.json')
+
+const convertToPercentage = (a, b) => {
+  const result = parseFloat(((a / b) * 100).toFixed(1))
+  return result ? `${result}%` : 'N/A'
+}
+
+const dimensions = [
+  { value: 'date', title: 'Date' },
+  { value: 'host', title: 'Host' },
+  { value: 'postal', title: 'Postal' },
+  { value: 'uaOS', title: 'OS' },
+]
+
+const reduce = function(row, memo) {
+  const { type } = row
+  switch (type) {
+    case 'impression':
+      return (memo.impressions = (memo.impressions || 0) + 1)
+    case 'display':
+      return (memo.displays = (memo.displays || 0) + 1)
+    case 'load':
+      return (memo.loads = (memo.loads || 0) + 1)
+    default:
+      return memo
+  }
+}
+
+const calculations = [
+  {
+    title: 'Display Rate',
+    value: memo => convertToPercentage(memo.displays, memo.loads),
+  },
+  {
+    title: 'Load Rate',
+    value: memo => convertToPercentage(memo.loads, memo.impressions),
+  },
+  { title: 'Impressions', value: 'impressions' },
+  { title: 'Displays', value: 'displays' },
+  { title: 'Loads', value: 'loads' },
+]
 
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
-  }
+  render() {
+    return (
+      <ReactPivot
+        rows={rows}
+        activeDimensions={['Date', 'Host']}
+        dimensions={dimensions}
+        reduce={reduce}
+        calculations={calculations}
+      />
+    )
+  },
 })
